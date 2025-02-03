c935b8126eedd4832669c7bf6be56341f8932cba
Darren Bansil <darrenbansil@gmail.com>
2025-02-03 15:49:00 -0500

    Add a file

---
+++ app/src/main/java/de/koelle/christian/trickytripper/activities/NewRandomActivity.java
@@ -0,0 +1,7 @@
+package de.koelle.christian.trickytripper.activities;
+
+public class NewRandomActivity {
+    public static void randomMethod() {
+        System.out.println("woah new method!");;
+    }
+}
